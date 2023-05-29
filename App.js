29ecfa27f80dc206cd93deb5671cd5022c4a1869
erarich <ericksantos@mailfence.com>
2023-05-29 16:01:27 -0300

    feat: [RF02, RF06, RF07, RF16, RF28] Adiciona saldo, valor gasto no mês, valor recebido no mês, botão para exportar conteúdo, botão para esconder saldo

--- App.js
+++ App.js
@@ -13,11 +13,30 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator                
           screenOptions={{
-              headerShown:false
+              headerShown:true, 
+              tabBarActiveTintColor: 'white',
+              tabBarActiveBackgroundColor: '#757de8',
+              tabBarInactiveTintColor: 'white',
+              tabBarStyle: {
+                backgroundColor: '#2196f3',
+                color: 'white',
+              }
           }}
           initialRouteName='Home'
       >
-          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen 
+            name="Home" 
+            component={Home} 
+            options={{
+              headerStyle: {
+                backgroundColor: '#2196f3',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          />
           <Tab.Screen name="Extrato" component={Extrato} />
           <Tab.Screen name="Contas" component={Contas} />
           <Tab.Screen name="Adicionar" component={Adicionar} />
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,41 @@
 import { View, Text, Button } from 'react-native';
 import styles from './styles.js'
+import { useState } from 'react';
 
 export default function Home() {
+
+    const [saldoEscondido, esconderSaldo] = useState(false);
+
+    const onPressEsconderSaldo = () => {
+        esconderSaldo(!saldoEscondido)
+    }
+
+    if (saldoEscondido === false){
+        saldo = <Text style={styles.text}>R$ 100,00</Text>
+    } else {
+        saldo = <View style={styles.rectangle}></View>
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Eu sou um texto!</Text>
+            <View style={styles.container}> 
+                <Button
+                    title="Exportar para PDF"
+                    color="#841504"
+                />
+                <Button
+                    onPress={onPressEsconderSaldo}
+                    title="Esconder Saldo"
+                    color="#841584"
+                />
+                <View style={styles.innerContainer}>
+                    {saldo}
+                    <View style={styles.receitaDespesaMensal}>
+                        <Text style={styles.text}>R$ 20,00</Text>
+                        <Text style={styles.text}>R$ 20,00</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
