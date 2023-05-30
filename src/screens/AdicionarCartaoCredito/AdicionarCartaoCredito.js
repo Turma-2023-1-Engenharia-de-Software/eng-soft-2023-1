c9b25b113106f51951441df82640e93ecafba47b
MateusAlvez <mateusrx1203@gmail.com>
2023-05-30 12:55:26 -0300

    Adicionando cartao de credito

--- src/screens/AdicionarCartaoCredito/AdicionarCartaoCredito.js
+++ src/screens/AdicionarCartaoCredito/AdicionarCartaoCredito.js
@@ -1,9 +1,61 @@
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from './styles'
 
-export default function AdicionarCartaoCredito() {
-    return (
-        <View style={{flex: 1, backgroundColor: '#000'}}>
-            <Text>Eu sou um texto!</Text>
-        </View>
-    );
+function AdicionarCartao() {
+  const [nome, setNome] = useState('');
+  const [numero, setNumero] = useState('');
+  const [vencimento, setVencimento] = useState('');
+
+  const handleAdicionarCartao = async () => {
+    if (nome && numero && vencimento) {
+      const cartao = {
+        nome,
+        numero,
+        vencimento,
+      };
+
+      try {
+        // Salvar os detalhes do cartão
+        // (lógica de salvamento aqui)
+
+        // Limpar os campos após adicionar o cartão
+        setNome('');
+        setNumero('');
+        setVencimento('');
+
+        console.log('Cartão adicionado com sucesso!');
+      } catch (error) {
+        console.log('Erro ao salvar o cartão:', error);
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Adicionar Cartão de Crédito</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        value={nome}
+        onChangeText={text => setNome(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="numero"
+        value={numero}
+        onChangeText={text => setNumero(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Vencimento"
+        value={vencimento}
+        onChangeText={text => setVencimento(text)}
+      />
+      <Button title="Adicionar" onPress={handleAdicionarCartao} />
+    </View>
+  );
 }
+
+export default AdicionarCartao;
