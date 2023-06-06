ad6a677837f19112a0ab9190cc8130eaf29eee9f
erarich <ericksantos@mailfence.com>
2023-06-06 07:49:05 -0300

    feat: adiciona arquivos de rotas

---
+++ src/routes/Extrato.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Extrato from '../screens/Extrato/Extrato'
+import DetalheExtrato from "../screens/DetalheExtrato/DetalheExtrato";
+
+const Stack = createStackNavigator();
+
+export function ExtratoRoute() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Extrato" component={Extrato} />
+      <Stack.Screen name="DetalheExtrato" component={DetalheExtrato} />
+    </Stack.Navigator>
+  );
+}
