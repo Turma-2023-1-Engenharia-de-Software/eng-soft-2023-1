597b2a35fffd981e601de13d7ececfc2f2fbbd02
98loann <98luan@gmail.com>
2023-06-10 10:30:20 -0300

    Botao de + e tela de add Conta Bancaria

--- src/routes/AdicionarConta.js
+++ src/routes/AdicionarConta.js
@@ -6,7 +6,7 @@ import AdicionarContaBancaria from "../screens/AdicionarContaBancaria/AdicionarC
 const Stack = createStackNavigator()
 export function AdicionarRoute (){
     return (
-    <Stack.Navigator screenOptions={{headerShown:false}}>
+    <Stack.Navigator screenOptions={{headerShown:true}}>
       <Stack.Screen name="MainContas" component={Contas} />
       <Stack.Screen name="AdicionarContaBancaria" component={AdicionarContaBancaria} />
     </Stack.Navigator>
